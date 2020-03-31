0c68649219cbcf122c82bc6e2acfa5a79e242beb
xunairxaman <zunairzaman@gmail.com>
2020-04-01 01:28:42 +0500

    db updated

--- db/migrate/20200331011953_create_shortened_urls.rb
+++ db/migrate/20200331011953_create_shortened_urls.rb
@@ -3,6 +3,7 @@ class CreateShortenedUrls < ActiveRecord::Migration
     create_table :shortened_urls do |t|
       t.text :original_url
       t.string :short_url
+      t.integer :clicks
       t.string :sanitize_url
       t.timestamps null: false
     end
